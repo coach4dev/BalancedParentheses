ae018675265d856813f0392532672cb97a98f15e
coach4dev <info@coach4dev.com>
2019-12-27 12:09:41 -0800

    Add project files.

---
+++ BalancedParentheses/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] expression1 = { '(', '{', '}', ')' };
+            char[] expression2 = { '(', '{', '}', ')', ']' };
+
+            Console.WriteLine("Is Expression1 Balanced? " + IsParanthesesBalanced(expression1));
+            Console.WriteLine("Is Expression2 Balanced? " + IsParanthesesBalanced(expression2));
+            Console.ReadLine();
+        }
+
+        private static bool IsParanthesesBalanced(char[] expression)
+        {
+            HashSet<char> openingParentheses = new HashSet<char>() { '(', '{', '[' };
+            HashSet<char> closingParentheses = new HashSet<char>() { ')', '}', ']' };
+            Dictionary<char, char> matchingPairs = GetMatchingPairsDict();
+
+            Stack bracketStack = new Stack();
+
+            foreach (char i in expression)
+            {
+                if (openingParentheses.Contains(i))
+                {
+                    bracketStack.Push(i);
+                    continue;
+                }
+                if (closingParentheses.Contains(i))
+                {
+                    if (bracketStack.IsEmpty())
+                        return false;
+
+                    char openingParenthesis = bracketStack.Pop();
+                    if (matchingPairs[openingParenthesis] != i)
+                        return false;
+                }
+            }
+
+            if (bracketStack.IsEmpty())
+                return true;
+            return false;
+        }
+
+        private static Dictionary<char, char> GetMatchingPairsDict()
+        {
+            Dictionary<char, char> matchingPairs = new Dictionary<char, char>();
+            matchingPairs.Add('(', ')');
+            matchingPairs.Add('{', '}');
+            matchingPairs.Add('[', ']');
+            return matchingPairs;
+        }
+    }
+}
---
+++ BalancedParentheses/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalancedParentheses
+{
+    class Stack
+    {
+        public static int MAX = 1000;
+        public static int MIN = -1;
+        public int top = MIN;
+        public char[] items = new char[MAX];
+
+        public void Push(char input)
+        {
+            if (top == MAX - 1)
+                throw new Exception("Trying to push to a full stack");
+            top++;
+            items[top] = input;
+        }
+
+        public char Pop()
+        {
+            if (top == MIN)
+                throw new Exception("Trying to pop an empty stack");
+
+            char result = items[top];
+            top--;
+            return result;
+        }
+
+        public bool IsEmpty()
+        {
+            if (top == MIN)
+                return true;
+
+            return false;
+        }
+    }
+}
